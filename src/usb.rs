a6aeebf682ee1051660bb72ca41af4b3529b2911
David Sawatzke <d-git@sawatzke.dev>
2025-12-26 15:54:02 +0100

    Move send assembly to rust

--- src/usb.rs
+++ src/usb.rs
@@ -212,6 +212,7 @@ impl<const USB_BASE: usize, const DP: u8, const DM: u8> UsbIf<USB_BASE, DP, DM> 
     // Probably not for the interrupt handler?
     pub(crate) fn make_funcs(&mut self) {
         unsafe { core::arch::asm!("// {}", sym Self::handle_se0_keepalive) }
+        unsafe { core::arch::asm!("// {}", sym Self::usb_send_empty) }
     }
     //#[unsafe(naked)]
     //#[unsafe(link_section = ".text.vector_handler")]
@@ -258,8 +259,8 @@ impl<const USB_BASE: usize, const DP: u8, const DM: u8> UsbIf<USB_BASE, DP, DM> 
             "li a5, 0xffffff07",
             "and a0, a0, a5 // Mask off non-HSI",
             // Decimate windup - use as HSIrim.
-            "neg a1, a1",
-            "srai a2, a1, 9",
+            "neg a1, a1
+            srai a2, a1, 9",
             "addi a2, a2, 16  // add HSI offset.",
             // Put trim in place in register.
             "slli a2, a2, 3",
@@ -271,5 +272,294 @@ impl<const USB_BASE: usize, const DP: u8, const DM: u8> UsbIf<USB_BASE, DP, DM> 
             DELTA_SE0_OFFSET = const mem::offset_of!(rv003usb_internal, delta_se0_cyccount),
         );
         // TODO get periph register addresses from/to proper addr
+    }
+    #[allow(no_mangle_generic_items)]
+    #[no_mangle]
+    #[allow(named_asm_labels)]
+    #[unsafe(naked)]
+    pub(crate) unsafe extern "C" fn usb_send_empty() {
+        // Also contains usb_send_data
+        core::arch::naked_asm!(
+            ".macro nx6p3delay_usb_send n, freereg",
+            "li \\freereg, ((\\n) + 1)",
+            "1: c.addi \\freereg, -1",
+            "c.bnez \\freereg, 1b",
+            ".endm",
+            // Needed ...
+            ".global usb_send_empty",
+            ".global usb_send_data",
+            ".balign 4",
+            //void usb_send_empty( uint32_t token );
+            "c.mv a3, a0",
+            "la a0, always0",
+            "li a1, 2",
+            "c.mv a2, a1",
+            //void usb_send_data( uint8_t * data, uint32_t length, uint32_t poly_function, uint32_t token );
+            "usb_send_data:",
+            "addi	sp,sp,-16",
+            "sw	s0, 0(sp)",
+            "sw	s1, 4(sp)",
+
+            "la a5, {USB_GPIO_BASE}",
+
+            // ASAP: Turn the bus around and send our preamble + token.
+            "c.lw a4, {CFGLR_OFFSET}(a5)",
+
+            "li s1, ~((0b1111<<({USB_PIN_DP}*4)) | (0b1111<<({USB_PIN_DM}*4)))",
+            "and a4, s1, a4",
+
+            // Convert D+/D- into 2MHz outputs
+            "li s1, ((0b0010<<({USB_PIN_DP}*4)) | (0b0010<<({USB_PIN_DM}*4)))",
+            "or a4, s1, a4",
+
+            "li s1, (1<<{USB_PIN_DP}) | (1<<({USB_PIN_DM}+16))",
+            "c.sw s1, {BSHR_OFFSET}(a5)",
+
+            //00: Universal push-pull output mode
+            "c.sw a4, {CFGLR_OFFSET}(a5)",
+
+            "li t1, (1<<{USB_PIN_DP}) | (1<<({USB_PIN_DM}+16)) | (1<<{USB_PIN_DM}) | (1<<({USB_PIN_DP}+16));",
+
+            //"SAVE_DEBUG_MARKER( 8 )
+
+            // Save off our preamble and token.
+            "c.slli a3, 7    ", //Put token further up so it gets sent later.
+            "ori s0, a3, 0x40",
+
+            "li t0, 0x0000",
+            "c.bnez a2, done_poly_check",
+            "li t0, 0xa001",
+            "li a2, 0xffff",
+            "done_poly_check:",
+
+            "c.slli a1, 3", // bump up one extra to be # of bits
+            "mv t2, a1",
+
+            // t0 is our polynomial
+            // a2 is our running CRC.
+            // a3 is our token.
+            //"DEBUG_TICK_SETUP",
+
+            "c.li a4, 6", // reset bit stuffing.
+            "c.li a1, 15", // 15 bits.
+
+            //c.nop; c.nop; c.nop;
+            "c.j pre_and_tok_send_inner_loop",
+
+            ////////////////////////////////////////////////////////////////////////////
+            // Send preamble + token
+            ".balign 4",
+            "pre_and_tok_send_inner_loop:",
+            /* High-level:
+            * extract the LSB from s0
+            * If it's 0, we FLIP the USB pin state.
+            * If it's 1, we don't flip.
+            * Regardless of the state, we still compute CRC.
+            * We have to decrement our bit stuffing index.
+            * If it is 0, we can reset our bit stuffing index.
+            */
+
+            // a3 is now the lsb of s0 (the 'next bit' to read out)
+            "c.mv a3, s0",
+            "c.srli s0, 1",  // Shift down into the next bit.
+            "c.andi a3, 1",
+            // If a3 is 0, we should FLIP
+            // if a3 is 1, we should NOT flip.
+
+            "c.addi a4, -1",
+            "c.bnez a3, pre_and_tok_send_one_bit",
+            //pre_and_tok_send_one_bit:
+            //Send 0 bit. (Flip)
+            // Flip s1 (our bshr setting) by xoring it.
+            // 10.....01
+            // 11.....11 (xor with)
+            // 01.....10
+            "xor s1, s1, t1",
+            "c.li a4, 6", // reset bit stuffing.
+            // DO NOT flip.  Allow a4 to increment.
+            // Deliberately unaligned for timing purposes.
+            ".balign 4",
+            "pre_and_tok_send_one_bit:",
+            "sw s1, {BSHR_OFFSET}(a5)",
+            //Bit stuffing doesn't happen.
+            "c.addi a1, -1",
+            "c.beqz a1, pre_and_tok_done_sending_data",
+            "nx6p3delay_usb_send 2, a3 ;	c.nop;            ", // Free time!
+            "c.j pre_and_tok_send_inner_loop",
+            ".balign 4",
+            "pre_and_tok_done_sending_data:",
+            ////////////////////////////////////////////////////////////////////////////
+
+            // We have very little time here.  Just enough to do this.
+
+            //Restore size.
+            "mv a1, t2//lw  a1, 12(sp)",
+            "c.beqz a1, no_really_done_sending_data ", //No actual payload?  Bail!
+            "c.addi a1, -1",
+            //	beqz t2, no_really_done_sending_data
+
+            "bnez t0, done_poly_check2",
+            "li a2, 0xffff",
+            "done_poly_check2:",
+
+
+            // t0 is used for CRC
+            // t1 is free
+            // t2 is a backup of size.
+
+            // s1 is our last "state\"
+            //    bit 0 is last "physical" state,
+            // s0 is our current "bit" / byte / temp.
+
+            // a0 is our data
+            // a1 is our length
+            // a2 our CRC
+            // a3 is TEMPORARY
+            // a4 is used for bit stuffing.
+            // a5 is the output address.
+
+            //xor s1, s1, t1
+            //c.sw s1, BSHR_OFFSET(a5)
+
+            // This creates a preamble, which is alternating 1's and 0's
+            // and then it sets the same state.
+            //	li s0, 0b10000000
+
+            //	c.j send_inner_loop
+            ".balign 4",
+            "load_next_byte:",
+            "lb s0, 0(a0)",
+            ".long 0x00150513", // addi a0, a0, 1  (For alignment's sake)
+            "send_inner_loop:",
+            /* High-level:
+            * extract the LSB from s0
+            * If it's 0, we FLIP the USB pin state.
+            * If it's 1, we don't flip.
+            * Regardless of the state, we still compute CRC.
+            * We have to decrement our bit stuffing index.
+            * If it is 0, we can reset our bit stuffing index.
+            */
+
+            // a3 is now the lsb of s0 (the 'next bit' to read out)
+            "c.mv a3, s0",
+            "c.andi a3, 1",
+            // If a3 is 0, we should FLIP
+            // if a3 is 1, we should NOT flip.
+            "c.beqz a3, send_zero_bit",
+            "c.srli s0, 1", // Shift down into the next bit.
+            //send_one_bit:
+            //HANDLE_CRC (1 bit)
+            "andi a3, a2, 1",
+            "c.addi a3, -1",
+            "and a3, a3, t0",
+            "c.srli a2, 1",
+            "c.xor a2, a3",
+
+            "c.addi a4, -1",
+            "c.beqz a4, insert_stuffed_bit",
+            "c.j cont_after_jump",
+            //Send 0 bit. (Flip)
+            ".balign 4",
+            "send_zero_bit:",
+            "c.srli s0, 1", // Shift down into the next bit.
+
+            // Handle CRC (0 bit)
+            // a2 is our running CRC
+            // a3 is temp
+            // t0 is polynomial.
+
+            // XXX WARNING: this was by https://github.com/cnlohr/rv003usb/issues/7
+            // TODO Check me!
+            "slli a3,a2,31", // Put a3s LSB into a0s MSB
+            "c.srai a3,31   ", // Copy MSB into all other bits
+
+            // Flip s1 (our bshr setting) by xoring it.
+            // 10.....01
+            // 11.....11 (xor with)
+            // 01.....10
+            "xor s1, s1, t1",
+            "sw s1, {BSHR_OFFSET}(a5)",
+
+            "c.li a4, 6", // reset bit stuffing.
+
+            // XXX XXX CRC down here to make bit stuffing timings line up.
+            "c.srli a2,1",
+            "and a3,a3,t0",
+            "c.xor  a2,a3",
+
+            ".balign 4",
+            "cont_after_jump:",
+            "send_end_bit_complete:",
+            "c.beqz a1, done_sending_data",
+            "andi a3, a1, 7",
+            "c.addi a1, -1",
+            "c.beqz a3, load_next_byte",
+            // Wait an extra few cycles.
+            "c.j 1f; 1:",
+            "c.j send_inner_loop",
+
+            ".balign 4",
+            "done_sending_data:",
+            // BUT WAIT!! MAYBE WE NEED TO CRC!
+            "beqz t0, no_really_done_sending_data",
+            "srli t0, t0, 8", // reset poly - we don't want it anymore.
+            "li a1, 7", // Load 8 more bits out
+            "beqz t0, send_inner_loop ", //Second CRC byte
+            // First CRC byte
+            "not s0, a2", // get read to send out the CRC.
+            "c.j send_inner_loop",
+
+
+            ".balign 4",
+            "no_really_done_sending_data:",
+
+            //	c.bnez a2, poly_function  TODO: Uncomment me!
+
+            "nx6p3delay_usb_send 2, a3 ;",
+
+            // Need to perform an SE0.
+            "li s1, (1<<({USB_PIN_DM}+16)) | (1<<({USB_PIN_DP}+16))",
+            "c.sw s1, {BSHR_OFFSET}(a5)",
+
+            "nx6p3delay_usb_send 7, a3 ;",
+
+            "li s1, (1<<({USB_PIN_DM})) | (1<<({USB_PIN_DP}+16))",
+            "c.sw s1, {BSHR_OFFSET}(a5)",
+
+            "lw s1, {CFGLR_OFFSET}(a5)",
+            // Convert D+/D- into inputs.
+            "li a3, ~((0b11<<({USB_PIN_DP}*4)) | (0b11<<({USB_PIN_DM}*4)))",
+            "and s1, a3, s1",
+            // 01: Floating input mode.
+            "li a3, ((0b01<<({USB_PIN_DP}*4+2)) | (0b01<<({USB_PIN_DM}*4+2)))",
+            "or s1, a3, s1",
+            "sw s1, {CFGLR_OFFSET}(a5)",
+
+            "lw	s0, 0(sp)",
+            "lw	s1, 4(sp)",
+            //"RESTORE_DEBUG_MARKER( 8 )",
+            "addi	sp,sp,16",
+            "ret",
+
+            ".balign 4",
+            // TODO: This seems to be either 222 or 226 (not 224) in cases.
+            // It's off by 2 clock cycles.  Probably OK, but, hmm.
+            "insert_stuffed_bit:",
+            "nx6p3delay_usb_send 3, a3",
+            "xor s1, s1, t1",
+            "c.li a4, 6", // reset bit stuffing.
+            "c.nop",
+            "c.nop",
+            "sw s1, {BSHR_OFFSET}(a5)",
+            "c.j send_end_bit_complete",
+            ".balign 4",
+            "always0:",
+            ".word 0x00",
+            USB_GPIO_BASE            = const USB_BASE,
+            USB_PIN_DP            = const DP,
+            USB_PIN_DM            = const DM,
+            BSHR_OFFSET            = const 16, // Don't see a good way to get this from the pac?
+            CFGLR_OFFSET            = const 0,
+        );
     }
 }
